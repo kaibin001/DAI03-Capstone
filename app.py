e9a950950f5bbd3f199daec469e16cf63d69a2f6
kaibin001 <158435985+kaibin001@users.noreply.github.com>
2024-04-30 17:03:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ def process_features(fighter1, fighter2):
     # This is just a placeholder
     features_f1 = fight_events.loc[fight_events['Fighter1']==fighter1]
     features_f1 = features_f1.iloc[:,7:20]
-    features_f1 = features_f1[0]
+    features_f1 = features_f1.iloc[0:1,:]
     features_f2 = fight_events.loc[fight_events['Fighter2']==fighter2]
     features_f2 = features_f2.iloc[:,20:]
-    features_f2 = features_f2[0]
+    features_f2 = features_f2.iloc[0:1,:]
     # features = pd.merge(features_f1,features_f2, on='Weight Class')
     features = pd.concat([features_f1,features_f2], axis=1, ignore_index = True)
     return features
