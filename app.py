6065491d1b70eb3089ddf92946cf0b43e40874c0
kaibin001 <158435985+kaibin001@users.noreply.github.com>
2024-04-30 17:21:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pickle
 
 # Load the model and data
-model = pickle.load(open('fight_model.pkl', 'rb'))
+model = pickle.load(open('fight_model2.pkl', 'rb'))
 fighter_details = pd.read_csv('File 3.csv')
 fight_events = pd.read_csv('new_fight_detail_full.csv')
 
@@ -12,12 +12,12 @@ def process_features(fighter1, fighter2):
     # You need to replace this part with actual feature processing based on your model
     # This is just a placeholder
     features_f1 = fight_events.loc[fight_events['Fighter1']==fighter1]
-    # features_f1 = features_f1.iloc[:,[4,5,7,8,9,10,11,12,13,14,15,16,17,18,19]]
-    features_f1 = features_f1[features_f1.columns([4,5,7,8,9,10,11,12,13,14,15,16,17,18,19])]
+    features_f1 = features_f1.iloc[:,[4,5,7,8,9,10,11,12,13,14,15,16,17,18,19]]
+    # features_f1 = features_f1[features_f1.columns([4,5,7,8,9,10,11,12,13,14,15,16,17,18,19])]
     features_f1 = features_f1.iloc[0:1,:]
     features_f2 = fight_events.loc[fight_events['Fighter2']==fighter2]
-    # features_f2 = features_f2.iloc[:,[4,5,20,21,22,23,24,25,26,27,28,29,30,31,32]]
-    features_f2 = features_f2[features_f2.columns([4,5,20,21,22,23,24,25,26,27,28,29,30,31,32])]
+    features_f2 = features_f2.iloc[:,[4,5,20,21,22,23,24,25,26,27,28,29,30,31,32]]
+    # features_f2 = features_f2[features_f2.columns([4,5,20,21,22,23,24,25,26,27,28,29,30,31,32])]
     features_f2 = features_f2.iloc[0:1,:]
     # features = pd.merge(features_f1,features_f2, on='Weight Class')
     features_f1.reset_index(drop=True, inplace=True)
