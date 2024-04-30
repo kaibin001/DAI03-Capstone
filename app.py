a9b4e784d92cbeb45f7346ae4758d885c7342695
kaibin001 <158435985+kaibin001@users.noreply.github.com>
2024-04-30 16:50:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ if st.button("Predict Winner"):
         input_features = process_features(fighter1, fighter2)
         st.write(fighter1)
         st.write(fighter2)
+        st.write(input_features)
         prediction = model.predict([input_features])
         winner = "Fighter 1 Wins" if prediction == 1 else "Fighter 2 Wins"
         st.success(winner)
