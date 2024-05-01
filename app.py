2d956fbb2ddc0b163da715916635840b9e284946
kaibin001 <158435985+kaibin001@users.noreply.github.com>
2024-05-01 11:32:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pickle
 
 # Load the model and data
-model = pickle.load(open('fight_model4.pkl', 'rb'))
+model = pickle.load(open('latest_model.pkl', 'rb'))
 fight_events = pd.read_csv('new_fight_detail_full.csv')
 
 
